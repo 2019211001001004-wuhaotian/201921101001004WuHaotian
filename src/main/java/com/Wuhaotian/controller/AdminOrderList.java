317caeedcf516a73074a05e2e20bba2faa8f0f59
2019211001001004-wuhaotian <2636381952@qq.com>
2021-06-17 22:10:39 +0800

    HW-homework 14 commit

---
+++ src/main/java/com/Wuhaotian/controller/AdminOrderList.java
@@ -0,0 +1,45 @@
+package com.Wuhaotian.controller;
+
+import com.Wuhaotian.dao.OrderDao;
+import com.Wuhaotian.model.Order;
+import com.Wuhaotian.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrderList", value = "/admin/orderList")
+public class AdminOrderList extends HttpServlet {
+    private Connection con= null;
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        con= (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList=Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao=new OrderDao();
+        List<Order> orderList=orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+
+}
